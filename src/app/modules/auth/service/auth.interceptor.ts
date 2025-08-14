2ac4125b3f9a5795c1a23de3847fbd34a48dafb6
ChristianNL <nsokengchristian@gmail.com>
2025-08-14 05:58:41 +0100

    Refactor auth interceptor to simplify token handling and remove unnecessary logout logic

--- src/app/modules/auth/service/auth.interceptor.ts
+++ src/app/modules/auth/service/auth.interceptor.ts
@@ -11,43 +11,11 @@ export const authInterceptor: HttpInterceptorFn = (
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (req.url.includes('/auth/login') || req.url.includes('/auth/register')) {
+  if (req.url.includes('/auth/')) {
     return next(req);
   }
 
   const token = authService.getToken();
-
-  // if (token) {
-  //   const authReq = req.clone({
-  //     setHeaders: {
-  //       Authorization: `Bearer ${token}`
-  //     }
-  //   });
-  //
-  //   return next(authReq).pipe(
-  //       catchError((error: HttpErrorResponse) => {
-  //         if (error.status === 401) {
-  //           const noLogoutEndpoints = [
-  //             '/change-password',
-  //             '/new',
-  //             '/edit'
-  //           ];
-  //
-  //           const shouldLogout = !noLogoutEndpoints.some(endpoint =>
-  //               req.url.includes(endpoint)
-  //           );
-  //
-  //           if (shouldLogout) {
-  //             authService.logout().subscribe(() => {
-  //               router.navigate(['/login']);
-  //             });
-  //           }
-  //         }
-  //
-  //         return throwError(() => error);
-  //       })
-  //   );
-  // }
 
   if (token) {
     const authReq = req.clone({
@@ -58,7 +26,6 @@ export const authInterceptor: HttpInterceptorFn = (
 
     return next(authReq).pipe(
         catchError((error: HttpErrorResponse) => {
-          // Vérifie si c'est une erreur 401 ET que c'est dû à un token invalide/expiré
           if (error.status === 401 && isTokenInvalidError(error)) {
             authService.logout().subscribe(() => {
               router.navigate(['/login']);
@@ -74,7 +41,6 @@ export const authInterceptor: HttpInterceptorFn = (
 };
 
 function isTokenInvalidError(error: HttpErrorResponse): boolean {
-  // Vérifie si l'erreur contient un message spécifique (ex: "Token expired")
   return (
       error.error?.message?.includes('Token') ||
       error.error?.error === 'Unauthorized' ||
